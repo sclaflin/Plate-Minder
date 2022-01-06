4d2faf8a8751bf37e89ac9620a2b0c06c1a1d11e
Sean Claflin <sean_claflin@protonmail.com>
2022-01-06 12:13:23 -0600

    Added support for a default config.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import YAML from 'yaml';
 import fs from 'fs/promises';
 import PlateMinder from './lib/PlateMinder.js';
+import { DEFAULT_CONFIG } from './lib/constants.js';
 
 (async () => {
 	const config = YAML.parse(
 		(await fs.readFile('config.yaml')).toString()
-	);
-	
+	) || DEFAULT_CONFIG;
+
 	PlateMinder.fromObject(config);
 })();
 
